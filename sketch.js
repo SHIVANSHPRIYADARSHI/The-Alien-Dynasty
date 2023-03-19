014dd5606d7854a7d71df2d0894f3e1fbb795beb
SHIVANSHPRIYADARSHI <96731380+SHIVANSHPRIYADARSHI@users.noreply.github.com>
2023-03-19 13:16:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var gameState = "wait"
+var bgimg, splashscreen
+
+
+function preload() {
+
+    bgimg = loadImage("alienDynasty.gif")
+
+
+
+}
+
+
+function setup() {
+    createCanvas(windowWidth, windowHeight)
+
+    // splashscreen = createSprite(width/2,height/2)
+    // splashscreen.addImage(bgimg)
+
+
+
+
+}
+
+
+function draw() {
+    background(bgimg)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
